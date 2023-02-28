bbdb9f4ec6948d7ccfe296bd4c494c3c47f8b220
Connor-Quinny <cmquinn98@gmail.com>
2023-02-28 05:30:05 +0000

    ReadMe updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Frost-RNG-Discord-Bot
+# RNG-Discord-Bot
+This project is a discord bot that uses a slash command for you to type a number, and it will choose a random number from 1 - your number, and will announce the winner as the first person to reply with the correct number.
+
+# Instructions
+```
+git clone https://github.com/Connor-Quinny/name
+cd name
+```
+- `npm install`
+- Create a `config.json` file 
+- Fill these values in your config file: 
+
+```
+{
+    "clientId": "your clientId", 
+    "guildId": "your guildId", 
+    "token": "your token"
+}
+```
+- add `config.json` to `.gitignore`
+- `node deployCommands.js`
+- `node index.js`
--- commands/rng.js
+++ commands/rng.js
@@ -12,9 +12,16 @@ module.exports = {
         const num = interaction.options.getInteger('number');
         const answer = (Math.floor(Math.random() * num) + 1);
         console.log(answer, num)
+
         const filter = response => {
             return parseInt(response.content) === answer
         }
+        if (!interaction.member.roles.cache.has('1077806635457708142')) {
+            return await interaction.reply({
+              content: 'You must have the admin role to use this command!',
+              ephemeral: true
+            });
+          }
         await interaction.reply({ content: `Guess the Number between 1 - ${num}`, fetchReply: true })
         .then(() => {
             interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
